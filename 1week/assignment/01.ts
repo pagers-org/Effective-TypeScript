c4d084bab22ac4acc2b5633d3c3b43b89aefb4eb
free-ko <duddnr654@naver.com>
2022-08-07 18:38:21 +0900

    [FEAT]: 과제1 제출

--- 1week/assignment/01.ts
+++ 1week/assignment/01.ts
@@ -1,7 +1,13 @@
 /**
  * 아래의 객체들을 동일하게 선언할 수 있는 타입을 작성해주세요.
  */
-type Person = {};
+type Person = {
+  name: string | { first: number; last: string };
+  age: number | string;
+  sex: string;
+  birth: Date | string | null;
+  death?: Date;
+};
 
 const personA: Person = {
   name: 'Alan Turing',
@@ -25,3 +31,7 @@ const personC: Person = {
   birth: null,
   death: new Date('1954/06/07'),
 };
+
+console.log(personA);
+console.log(personB);
+console.log(personC);
