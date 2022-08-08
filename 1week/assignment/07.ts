b969fc2df2cd89ddffa12548c6eb5f546cc93ff1
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-08-08 16:12:52 +0900

    Update 07.ts

--- 1week/assignment/07.ts
+++ 1week/assignment/07.ts
@@ -2,9 +2,16 @@
  * 아래의 코드 중 Cylinder 타입을 유지하기 위한 코드를 작성해주세요.
  */
 
-interface Cylinder {
-  radius: number;
-  height: number;
+// interface Cylinder {
+//   radius: number;
+//   height: number;
+// }
+
+class Cylinder {
+    constructor(public height: number, public radius: number){
+        this.height = height;
+        this.radius = radius;
+    }
 }
 
 function calculateVolumne(shape: unknown) {
