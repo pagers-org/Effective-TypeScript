8ddd595977ebb476a59ba8fa99304e5fe9cfbef8
eden.ju <public.jin03@gmail.com>
2022-08-08 19:59:07 +0900

    feat: 04번 풀이

--- 1week/assignment/04.ts
+++ 1week/assignment/04.ts
@@ -5,14 +5,28 @@
  * - 특정 index의 원소를 변경하는 update 메서드를 만들어주세요.
  */
 
-class List {
-  private items: any[] = [];
+class List<T> {
+  private items:T[] = [];
 
-  add(item: any) {
+  add(item: T) {
     this.items.push(item);
   }
 
   get(index: number) {
     return this.items[index];
+  }
+
+  remove(index:number){
+    const front = this.items.slice(0, index);
+    const back = this.items.slice(index + 1);
+
+    this.items = [...front, ...back];
+  }
+
+  update(index:number, newItem:T){
+    const front = this.items.slice(0, index);
+    const back = this.items.slice(index + 1);
+
+    this.items = [...front, newItem,...back]
   }
 }
