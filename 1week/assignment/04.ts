5931ae3af691db3bc541ca7f2da42d6e127a4bf0
ganeodolu <ganeodolu@gmail.com>
2022-08-11 19:22:03 +0900

    feat: 4번 풀이 추가(제네릭)

--- 1week/assignment/04.ts
+++ 1week/assignment/04.ts
@@ -5,14 +5,27 @@
  * - 특정 index의 원소를 변경하는 update 메서드를 만들어주세요.
  */
 
-class List {
-  private items: any[] = [];
+class List<T> {
+  private items: Array<T> = [];
 
-  add(item: any) {
+  add(item: T) {
     this.items.push(item);
   }
 
   get(index: number) {
     return this.items[index];
   }
+  remove(index: number) {
+    return this.items = [...this.items].filter((_, filterIndex: number) => index !== filterIndex)
+  }
+  update(index: number, item: T) {
+    return this.items[index] = item;
+  }
 }
+const list = new List();
+console.log(list.add(0))
+console.log(list.add(4))
+console.log(list.add('333'))
+console.log(list.get(2))
+console.log(list.remove(0))
+console.log(list.update(0, 'test'))
