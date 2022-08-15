af4a8d376cc1037c3b4b1270a38d4a721a73ba64
semyung oh <semyung.dev@gmail.com>
2022-08-16 00:17:06 +0900

    [Feat] finish assignment 06

--- 1week/assignment/06.ts
+++ 1week/assignment/06.ts
@@ -1,23 +1,17 @@
 /**
  * 아래의 코드 중 타입 별칭을 제거하여 동일한 결과를 내도록 작성해주세요.
+ * 문제의 의도: discriminated union, typeof Value in type scope.
  */
 
-type SavingAction = {
-  type: 'saving';
-  payload: string[];
-};
-
-const savingAction: SavingAction = {
+const savingAction = {
   type: 'saving',
   payload: ['Apple', 'Banana', 'Strawberry'],
 };
 
-type SavedAction = {
-  type: 'saved';
-};
 
-const savedAction: SavedAction = {
+
+const savedAction = {
   type: 'saved',
 };
 
-type Actions = SavingAction | SavedAction;
+type Actions = typeof savedAction | typeof savingAction;
