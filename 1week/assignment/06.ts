727e157a12cd99f96eb4c18a877d3ecd28da25d6
semyung oh <semyung.dev@gmail.com>
2022-08-16 00:30:32 +0900

    [CHORE] comment on assignment 06

--- 1week/assignment/06.ts
+++ 1week/assignment/06.ts
@@ -15,3 +15,13 @@ const savedAction = {
 };
 
 type Actions = typeof savedAction | typeof savingAction;
+
+// 객체의 프로퍼티는 변경될 수 있기 때문에 literal 타입이 넓혀지는 사이드 이펙트가 생길 수 있다.
+// 따라서 아래의 변수는 할당된다.
+
+/**
+ * 
+ * const steven: Actions = {
+ *   type: steven
+ * }
+ */
