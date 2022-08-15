f43ea868578e5ae9986ca910496dc83820bc4e9a
wooooooood <13pft13@gmail.com>
2022-08-15 20:11:39 +0900

    Q 1,3,4,6,7,9

--- 1week/assignment/01.ts
+++ 1week/assignment/01.ts
@@ -1,7 +1,20 @@
 /**
  * 아래의 객체들을 동일하게 선언할 수 있는 타입을 작성해주세요.
  */
-type Person = {};
+type Person = {
+  name: string | TwoWordsNameType;
+  age: string | number;
+  sex: SexType;
+  birth: Date | string | null;
+  death?: Date;
+};
+
+type TwoWordsNameType = {
+  first: string | number;
+  last: string;
+}
+
+type SexType = "male" | "female" | '남자' | '여자'
 
 const personA: Person = {
   name: 'Alan Turing',
