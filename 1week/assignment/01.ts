1f7db2352634b2adc1eabaa179a125a9932e40bd
herekim <khere220@gmail.com>
2022-08-17 14:19:09 +0900

    feat: add assignment1

--- 1week/assignment/01.ts
+++ 1week/assignment/01.ts
@@ -1,7 +1,18 @@
 /**
  * 아래의 객체들을 동일하게 선언할 수 있는 타입을 작성해주세요.
  */
-type Person = {};
+type FirstLast = {first: number; last: string}
+type Name = string | FirstLast
+type Sex = 'male' | 'female' | '남자' | '여자'
+type BirthDeath = Date | '옛날 옛적에' | null
+
+type Person = {
+  name: Name,
+  age: string | number,
+  sex: Sex,
+  birth: BirthDeath,
+  death?: BirthDeath
+};
 
 const personA: Person = {
   name: 'Alan Turing',
