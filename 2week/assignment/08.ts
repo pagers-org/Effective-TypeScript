aa6c9840a72b9b1e82d4583c95fdb67dc2b84beb
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-08-21 19:46:58 +0900

    Update 08.ts

--- 2week/assignment/08.ts
+++ 2week/assignment/08.ts
@@ -1,8 +1,11 @@
 // 8. 다음 함수에서 매개 변수의 타입이 추론되도록 문맥을 만들어주세요.
-const callBackFn = (a:string, b:string) => {
+
+type callBackType = (a:string, b:string) => string;
+
+const callBackFn :callBackType= (a, b) => {
   return `These are ${a} and ${b}`;
 };
 
-const someFn = (callback: (a: string, b: string) => string) => {
+const someFn = (callback: callBackType) => {
   return callback('apple', 'banana');
 };
