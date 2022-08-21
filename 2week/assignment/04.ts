35ae34ff8a543221d95825bf61ebbb31db49d8ce
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-08-21 18:11:27 +0900

    Update 04.ts

--- 2week/assignment/04.ts
+++ 2week/assignment/04.ts
@@ -1,12 +1,11 @@
 // 4. 다음은 잘못된 타입 좁히기의 예시입니다. 올바르게 고쳐주세요.
 function print(strs: string | string[] | null) {
-  if (strs) {
-    if (typeof strs === 'object') {
+  if(!strs) return;
+  if (Array.isArray(strs)) { //string[]
       for (const s of strs) {
         console.log(s);
       }
-    } else if (typeof strs === 'string') {
+    } else { //string
       console.log(strs);
     }
-  }
 }
