a01d1aee9f254c8be19aaac2ab0a950333ccc351
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-08-21 16:56:55 +0900

    Update 01.ts

--- 2week/assignment/01.ts
+++ 2week/assignment/01.ts
@@ -1,8 +1,12 @@
+
 // 1. 타입스크립트가 다음을 타입추론 할 수 있게 바꿔주세요.
+type Gender = 'male' |'female';
+
 interface Person {
   name: string;
   age: number;
   country: string;
+  sex: Gender
 }
 
 const person: Person = {
