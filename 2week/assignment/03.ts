7c5a9415b9a0c8859b7ade35ad37728e9d3251e9
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-08-21 17:07:20 +0900

    Update 03.ts

--- 2week/assignment/03.ts
+++ 2week/assignment/03.ts
@@ -1,6 +1,12 @@
 // 3. 다음을 오류가 없도록 해주세요.
+type Size = 'Small' | 'Medium' | 'Large';
+interface Coffee{
+  name: string;
+  price:number;
+  size: Size;
+}
 const mycoffee = {
   name: 'mocha',
-};
+} as Coffee;
 mycoffee.price = 6000;
 mycoffee.size = 'Large';
