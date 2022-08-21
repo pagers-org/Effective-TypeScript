0531b2edb71286aa5648817e45d24b4e7f4b6af1
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-08-21 19:50:19 +0900

    Update 03.ts

--- 2week/assignment/03.ts
+++ 2week/assignment/03.ts
@@ -10,3 +10,12 @@ const mycoffee = {
 } as Coffee;
 mycoffee.price = 6000;
 mycoffee.size = 'Large';
+
+//--------------- 책에서는 객체를 한번에 선언하는 방법이 좋다고 하였는데 더 좋은 방법은 아래처럼 선언하는 방식을 사용해야 할까요?
+
+const mycoffe: Coffee {
+ name: 'mocha',
+ price: 6000,
+ size: 'Large'
+}
+  
