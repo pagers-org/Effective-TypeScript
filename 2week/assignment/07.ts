57a897a0de22244a3a4a0fe7a08cb010da8ab5f4
semyung oh <semyung.dev@gmail.com>
2022-08-22 22:07:57 +0900

    [FEAT] finish assignment 07

--- 2week/assignment/07.ts
+++ 2week/assignment/07.ts
@@ -1,2 +1,15 @@
 // 7. 다음에서 falsy 값만 분리해보세요.
-const arr = [0, 1, '', undefined, false, 2, undefined, null, -1, 3, NaN];
+
+{
+  // Typescript doesn't support NaN type. So we cannot filter NaN stacially.
+  // - https://github.com/Microsoft/TypeScript/issues/15135
+  type Falsy<T> = undefined extends T ? T : null extends T ? T : 0 extends T ? T : "" extends T ? T : false extends T ? T : never;
+  const arr = [0, 1, '', undefined, false, 2, undefined, null, -1, 3, NaN];
+  
+  function isFalsy<T>(val: T): val is Falsy<T> {
+    return !val;
+  }
+
+  const filteredArr = arr.filter(isFalsy);
+  console.log(filteredArr);
+}
