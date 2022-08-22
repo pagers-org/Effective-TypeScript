55717cb57ca6d6c9c823ed6a337d1d7dd0f37f03
siny <dltlsgh5@gmail.com>
2022-08-22 10:18:12 +0900

    [2주차과제] 3번문제 정답수정

--- 2week/assignment/03.ts
+++ 2week/assignment/03.ts
@@ -1,22 +1,15 @@
 // 3. 다음을 오류가 없도록 해주세요.
 type Coffee = {
   name: string;
-  price?: number;
-  size?: string;
+  price: number;
+  size: 'Large' | 'Medium' | 'Small';
 };
+
+// 타입선언은 변수생성시 한번에 하는것이 좋다!
 const mycoffee: Coffee = {
   name: 'mocha',
+  price: 6000,
+  size: 'Large',
 };
 
-mycoffee.price = 6000;
-mycoffee.size = 'Large';
-
-// 질문
-// type Coffee = {
-//   name: string;
-//   price?: number;
-//   size?: 'Large'|'Medium'|'Small';
-// };
-// 로 하는것과 size?: string 하는것 뭐가 더 좋을까요? 저는 보통 리터럴타입을 명시해서 쓰긴 하는데 타입관리하는데 있어서 까다롭더라구요...
-//
 export default {};
