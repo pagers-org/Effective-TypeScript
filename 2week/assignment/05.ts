9f12459626e381a1891b663c2a96f65e6b47ae4f
semyung oh <semyung.dev@gmail.com>
2022-08-22 21:58:05 +0900

    [FEAT] finish assignment 05

--- 2week/assignment/05.ts
+++ 2week/assignment/05.ts
@@ -5,14 +5,43 @@ type Bird = { fly: () => void };
 declare function getSmallPet(): Fish | Bird;
 
 // fish인지 판별 할 커스텀 함수를 작성해주세요.
-function isFish(pet) {}
 
-const pet = getSmallPet();
+function isFish__UN_SAFE(pet: Fish | Bird): pet is Fish {
+  return 'swim' in pet;
+}
 
-const move = (pet: Fish | Bird) => {
-  if (isFish(pet)) {
+const move__UN_SAFE = (pet: Fish | Bird) => {
+  if (isFish__UN_SAFE(pet)) {
     pet.swim();
   } else {
     pet.fly();
   }
 };
+
+function isFish__SAFE(pet: Fish | Bird): pet is Fish {
+  return Object.prototype.hasOwnProperty.call(pet, 'swim');
+}
+const move__SAFE = (pet: Fish | Bird) => {
+  if (isFish__SAFE(pet)) {
+    pet.swim();
+  } else {
+    pet.fly();
+  }
+};
+
+const fish: Fish = {
+  swim() {
+    console.log('swimming')
+  }
+}
+
+const bird: Bird = {
+  fly() {
+    console.log('fly')
+  }
+}
+
+Object.setPrototypeOf(bird, fish);
+
+move__SAFE(bird)
+move__UN_SAFE(bird)
