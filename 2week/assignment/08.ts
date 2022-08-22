deb6f47477838e8425fece8d3b98070b768f146c
semyung oh <semyung.dev@gmail.com>
2022-08-22 22:08:39 +0900

    [FEAT] finish assignment 08

--- 2week/assignment/08.ts
+++ 2week/assignment/08.ts
@@ -1,8 +1,12 @@
 // 8. 다음 함수에서 매개 변수의 타입이 추론되도록 문맥을 만들어주세요.
-const callBackFn = (a, b) => {
-  return `These are ${a} and ${b}`;
-};
+{
+  type Cb = (a: string, b: string) => string;
 
-const someFn = (callback: (a: string, b: string) => string) => {
-  return callback('apple', 'banana');
-};
+  const callBackFn: Cb = (a, b) => {
+    return `These are ${a} and ${b}`;
+  };
+  
+  const someFn = (callback: Cb) => {
+    return callback('apple', 'banana');
+  };
+}
