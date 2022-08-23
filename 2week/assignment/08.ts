1bbf817058e130cea8116f45f79c3c38787f5544
free-ko <duddnr654@naver.com>
2022-08-23 16:40:49 +0900

    [FEAT]: 과제2-8 제출

--- 2week/assignment/08.ts
+++ 2week/assignment/08.ts
@@ -1,5 +1,8 @@
 // 8. 다음 함수에서 매개 변수의 타입이 추론되도록 문맥을 만들어주세요.
-const callBackFn = (a, b) => {
+
+type callBackFnType = (a:string, b:string) => string
+
+const callBackFnL:callBackFnType = (a, b) => {
   return `These are ${a} and ${b}`;
 };
 
