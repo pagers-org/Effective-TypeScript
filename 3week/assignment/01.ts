0e6fde5e973ef344ff2a14da418e693368e1ffa7
eden.ju <public.jin03@gmail.com>
2022-08-30 00:12:14 +0900

    doc: 3주차 1번 주석 추가

--- 3week/assignment/01.ts
+++ 3week/assignment/01.ts
@@ -6,3 +6,6 @@
  * */
 type PapagoParamsSource = "ko" | "en" | "ja" | "zh-CN" | "zh-TW" | "vi" | "id" | "th" | "de" | "ru" | "es" | "it" | "fr";
 type PapagoParamsTarget = PapagoParamsSource;
+
+// 1. 파파고 api 명세를 확인했을 때 언어코드에 적힌 내용이 Source와 Target에 들어갈 것으로 보입니다. 
+// 2. 따라서 둘은 언어 코드의 유니온으로 이루어진 타입으로 좁힐 수 있다고 생각했습니다.
