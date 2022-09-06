eaf10f96ec271618366a66ef3ff5a6cb476c2276
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-09-06 16:20:26 +0900

    Update 02.ts

--- 3week/assignment/02.ts
+++ 3week/assignment/02.ts
@@ -3,14 +3,14 @@
  * 위의  PapagoParamsSource와 PapagoParamsTarget 를 조합하여 명세에서 언급하는
  * "번역할 수 있는 원본 언어와 목적 언어는 다음과 같습니다." 의 제약조건을 참고하여 PapagoParams 인터페이스를 개선해 주세요.
  * */
-type OtherLanguageCode =  'vi' | 'id' | 'th'| 'de' | 'ru' |'es' | 'it';
-type LanguageCode = 'ko' | 'en' |  'ja' | 'zh-CN' | 'zh-TW' | 'fr' | OtherLanguageCode;
+type LanguageCode = 'ko' | 'en' |  'ja' | 'zh-CN' | 'zh-TW' | 'fr' | 'vi' | 'id' | 'th'| 'de' | 'ru' |'es' | 'it';
 
 type EnPapagoParamsTarget = 'ja' | 'zh-CN' | 'zj-TW' | 'fr' | 'ko';
 type JaPapagoParamsTarget = 'zh-CN' | 'zh-TW' | 'ko' | 'en';
 type ZH_CNPapagoParamsTarget = 'zh-TW' | 'ko' | 'en' | 'ja';
 type ZH_TWPapagoParamsTarget = 'zh-CN' |'ko' | 'en' | 'ja';
 type FrPapagoParamsTarget = 'ko' | 'en';
+
 
 interface KoPapagoParams {
   source: 'ko';
@@ -42,11 +42,41 @@ interface FrPapagoParams {
   target: FrPapagoParamsTarget;
 }
 
-interface OtherLangPapagoParams {
-    source: OtherLanguageCode;
+interface VIPapagoParams {
+    source: 'vi';
+    target: 'ko';
+}
+
+interface IDPapagoParams {
+    source: 'id';
+    target: 'ko';
+}
+
+interface THPapagoParams {
+    source: 'th';
+    target: 'ko';
+}
+
+interface DEPapagoParams {
+    source: 'de';
+    target: 'ko';
+}
+
+interface RUPapagoParams {
+    source: 'ru';
+    target: 'ko';
+}
+
+interface ESPapagoParams {
+    source: 'es';
+    target: 'ko';
+}
+
+interface ITPapagoParams {
+    source: 'it';
     target: 'ko';
 }
 
 
-type PapagoParams = KoPapagoParams | EnPapagoParams | JaPapagoParams | ZH_CNPapagoParams | ZH_TWPapagoParams | FrPapagoParams | OtherLangPapagoParams;
+type PapagoParams = KoPapagoParams | EnPapagoParams | JaPapagoParams | ZH_CNPapagoParams | ZH_TWPapagoParams | FrPapagoParams | VIPapagoParams | IDPapagoParams | THPapagoParams | DEPapagoParams | RUPapagoParams | ESPapagoParams | ITPapagoParams;
 
